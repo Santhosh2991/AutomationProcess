4e60adbd9726d7231c320a5f215532ff00194878
Sandyboy <Sandyboy@DESKTOP-55Q5DIE>
2019-11-06 22:20:25 +0530

    s

--- Day46GitPro/src/SandyGit.java
+++ Day46GitPro/src/SandyGit.java
@@ -4,7 +4,7 @@ public class SandyGit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Git INtegration");
-System.out.println("git 2");
+System.out.println("git 23");
 	}
 
 }
